2d21f935c5740406bbfbff0121861a4040161a87
袁澧林 <kobe980918@gmail.com>
2023-07-03 17:30:28 +0800

    路由管理

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+// 路由配置
+const router = createRouter({
+  history: createWebHashHistory(),
+  // 映射关系：path -> componen
+  routes: [
+    {
+      path: '/',
+      redirect: '/home' //重定向
+    },
+    {
+      path: '/home',
+      component: () => import("@/views/home/home.vue")
+    },
+    {
+      path: '/favor',
+      component: () => import("@/views/favor/favor.vue")
+    },{
+      path: '/message',
+      component: () => import("@/views/message/message.vue")
+    },{
+      path: '/order',
+      component: () => import("@/views/order/order.vue")
+    },
+  ]
+})
+
+export default router
