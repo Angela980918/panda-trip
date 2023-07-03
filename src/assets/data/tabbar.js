0ba0f5c833ba332ee323d3c7d672f7faf40168c1
袁澧林 <kobe980918@gmail.com>
2023-07-03 17:31:00 +0800

    封装tabbar组件

---
+++ src/assets/data/tabbar.js
@@ -0,0 +1,28 @@
+const tabbarData = [
+  {
+    text: "首页",
+    image: "home.png",
+    imageActive: "home_active.png",
+    path: '/home'
+  },
+  {
+    text: "收藏",
+    image: "favor.png",
+    imageActive: "favor_active.png",
+    path: '/favor'
+  },
+  {
+    text: "订单",
+    image: "order.png",
+    imageActive: "order_active.png",
+    path: '/order'
+  },
+  {
+    text: "信息",
+    image: "message.png",
+    imageActive: "message_active.png",
+    path: '/message'
+  }
+]
+
+export default tabbarData
---
+++ src/components/tab-bar/tab-bar.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="tab-bar">
+    <!-- <router-link to="/home">首页</router-link>
+    <router-link to="/favor">收藏</router-link>
+    <router-link to="/order">订单</router-link>
+    <router-link to="/message">消息</router-link> -->
+    <template v-for="(item, index) in tabbarData" :key="index">
+      <div class="tab-bar-item" :class="{ active: currentIndex === index }">
+        <img :src="getAssetUrl(item.image)" alt="">
+        <div class="text">{{ item.text }}</div>
+      </div>
+    </template>
+
+  </div>
+</template>
+
+<script setup>
+import tabbarData from '@/assets/data/tabbar.js'
+import { getAssetUrl } from '@/utils/get_img.js'
+import { ref } from 'vue';
+
+
+const currentIndex = ref(0)
+
+
+
+
+</script>
+
+<style lang="less" scoped>
+.tab-bar {
+  // background-color: aqua;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  height: 55px;
+
+  // flex布局,
+  display: flex;
+
+  // 浅浅的上边缘
+  border-top: 1px solid #000000;
+
+
+  .tab-bar-item {
+    // 每一个item各占1个位置->平分所有位置
+    flex: 1;
+
+    // 对于每个item里面的img和text
+    // 要求上下排列,要求他们居中,所以flex
+    display: flex;
+    flex-direction: column;
+
+    // align-items会让元素在Y轴对齐
+    align-items: center;
+
+    // x轴方向上居中
+    justify-content: center;
+
+    //icon大小
+    img {
+      height: 36px;
+    }
+
+    //icon文本
+    .text {
+      font-size: 12px;
+    }
+
+  }
+}
+</style>
