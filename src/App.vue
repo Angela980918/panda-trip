41d906a5f47b385a36e3a6996a4a2bf8cccbdb07
袁澧林 <kobe980918@gmail.com>
2023-07-03 17:31:32 +0800

    引入tabbra组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
+  <div class="app">
     初始化项目
+    <router-view></router-view>
+  <tab-bar></tab-bar>
   </div>
 </template>
 
 <script setup>
 
+  import TabBar from "./components/tab-bar/tab-bar.vue"
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
