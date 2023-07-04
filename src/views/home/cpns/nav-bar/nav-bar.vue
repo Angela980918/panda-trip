f0b685e4072835e77fcefd1e58ab1064fda900f2
袁澧林 <kobe980918@gmail.com>
2023-07-04 17:20:28 +0800

    nav-bar组件

---
+++ src/views/home/cpns/nav-bar/nav-bar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="nav-bar">
+    <img src="/public/favicon.ico" alt="">
+    <span>熊猫旅途</span>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="less" scoped>
+.nav-bar {
+  height: 46px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    height: 40px;
+    border-radius: 50%;
+  }
+
+  span {
+    margin-left: 6px;
+    font-size: 18px;
+    font-weight: bold;
+    font-family: "YouYuan", "幼圆", "圆体", cursive;
+  }
+}
+</style>
