571ba1f0ab0ba467e78e29999f08431725eef4b0
袁澧林 <kobe980918@gmail.com>
2023-07-18 17:28:56 +0800

    更新 format_date.js

--- src/utils/format_date.js
+++ src/utils/format_date.js
@@ -5,6 +5,10 @@ export function formatDate(date) {
   return dayjs(date).format("MM月DD日");
 }
 
+export function formatDate2(date) {
+  return dayjs(date).format("MM.DD");
+}
+
 // 计算入住和离店的时间差（天数）
 export function getDiffDate(start, end) {
   // console.log(start,end);
