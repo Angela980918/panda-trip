e4a8841acf138b5508f2a25e9ef9c938420e4c19
袁澧林 <kobe980918@gmail.com>
2023-07-19 17:26:21 +0800

    更新 home-content.vue

--- src/views/home/cpns/home-content/home-content.vue
+++ src/views/home/cpns/home-content/home-content.vue
@@ -2,12 +2,13 @@
   <div class="content">
     <div class="list">
       <template v-for="item in houseData" :key="item.data.houseId">
-        <house-item-v9 v-if="item.discoveryContentType === 9" :item="item.data"/>
-        <house-item-v3 v-else-if="item.discoveryContentType === 3" :item="item.data"/>
+        <house-item-v9 v-if="item.discoveryContentType === 9" :item="item.data"
+          @click="houseDetailClick(item.data.houseId)" />
+        <house-item-v3 v-else-if="item.discoveryContentType === 3" :item="item.data"
+          @click="houseDetailClick(item.data.houseId)" />
       </template>
     </div>
   </div>
-
 </template>
 
 <script setup>
@@ -15,19 +16,21 @@ import houseItemV3 from '@/components/home-house-item/house-item-v3.vue'
 import houseItemV9 from '@/components/home-house-item/house-item-v9.vue'
 import useHomeStore from '@/store/modules/home'
 import { storeToRefs } from "pinia"
+import { useRouter } from 'vue-router';
 
 const homeStore = useHomeStore()
 const { houseData } = storeToRefs(homeStore)
+const router = useRouter()
 
-const houseLsitClick = () => {
-  console.log("房屋列表点击");
-  homeStore.fetchAllHouse();
+const houseDetailClick = (houseID) => {
+  console.log("房屋详情:", houseID);
+  router.push('/detail/'+ houseID)
 }
 </script>
 
 <style lang="less" scoped>
-.content{
-  .list{
+.content {
+  .list {
     display: flex;
     flex-wrap: wrap;
     margin-top: 10px;
