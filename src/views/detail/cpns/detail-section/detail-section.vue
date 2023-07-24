3a9d6cd3a4545b85b1e59216318c0c4c0435c9e3
袁澧林 <kobe980918@gmail.com>
2023-07-24 17:24:14 +0800

    创建 detail-section.vue
    封装detail-section组件，预留插槽

---
+++ src/views/detail/cpns/detail-section/detail-section.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="section">
+    <div class="header">
+      <h2 class="title">title</h2>
+    </div>
+    <div class="content">
+      <slot>默认内容</slot>
+    </div>
+    <div class="footer">
+      <span class="more">
+        更多
+        <van-icon name="arrow" />
+      </span>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+.section {
+  margin-top: 10px;
+  padding: 0 16px;
+  border-top: 10px solid #eee;
+
+  .header {
+    height: 52px;
+    line-height: 52px;
+
+    .title {
+      color: #333;
+      font-size: 20px;
+      font-weight: 600;
+    }
+  }
+
+  .content{
+    padding: 8px 0;
+  }
+
+  .footer {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    padding: 0 16px;
+    color: var(--primary-color);
+    font-weight: 600;
+  }
+}
+</style>
