623c168030aa4baafb21b862d0559906a2743b7a
袁澧林 <kobe980918@gmail.com>
2023-07-27 17:16:12 +0800

    更新 useScroll.js
    优化使用

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,9 +1,12 @@
 import { onMounted, onUnmounted } from "vue";
 import { ref } from "vue";
-import { throttle } from "underscore"; 
+import { throttle } from "underscore";
 
 // 封装滚动到底部自动加载的函数
-export default function useScroll() {
+export default function useScroll(elRef) {
+  // 默认监听window
+  let el = window;
+
   // 初始默认为没有到底
   const isReachBottom = ref(false);
   const scrollTop = ref(0);
@@ -11,12 +14,24 @@ export default function useScroll() {
   const clientHeight = ref(0);
 
   const scrollListener = throttle(() => {
-    // 文档距离窗口顶部的距离
-    scrollTop.value = document.documentElement.scrollTop;
-    // 页面总体长度
-    scrollHeight.value = document.documentElement.scrollHeight;
-    // 屏幕的长度
-    clientHeight.value = document.documentElement.clientHeight;
+    // console.log("正在滚动");
+
+    if (el === window) {
+      // 文档距离窗口顶部的距离
+      scrollTop.value = document.documentElement.scrollTop;
+      // 页面总体长度
+      scrollHeight.value = document.documentElement.scrollHeight;
+      // 屏幕的长度
+      clientHeight.value = document.documentElement.clientHeight;
+    } else {
+      // 元素距离窗口顶部的距离
+      scrollTop.value = el.scrollTop;
+      // 元素距离窗口顶部的距离总体长度
+      scrollHeight.value = el.scrollHeight;
+      // 元素的长度
+      clientHeight.value = el.clientHeight;
+      // console.log(scrollTop.value, scrollHeight.value, clientHeight.value);
+    }
     if (clientHeight.value + scrollTop.value + 0.1 >= scrollHeight.value) {
       // homeStore.fetchAllHouse();
       console.log("加载更多");
@@ -25,11 +40,14 @@ export default function useScroll() {
   }, 500);
 
   onMounted(() => {
-    window.addEventListener("scroll", scrollListener);
+    if (elRef) {
+      el = elRef.value;
+    }
+    el.addEventListener("scroll", scrollListener);
   });
 
   onUnmounted(() => {
-    window.removeEventListener("scroll", scrollListener);
+    el.removeEventListener("scroll", scrollListener);
   });
 
   return { isReachBottom, scrollTop, scrollHeight, clientHeight };
