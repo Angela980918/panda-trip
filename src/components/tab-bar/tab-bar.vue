3ed48a081428937b6d49da5c5710ec61f370d9eb
袁澧林 <kobe980918@gmail.com>
2023-07-27 17:15:48 +0800

    更新 tab-bar.vue

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="tab-bar">
-    <van-tabbar route v-model="currentIndex" active-color="#3cb490" inactive-color="#000000" placeholder>
-      <van-tabbar-item v-for="(item, index) in tabbarData" :key="index" :to="item.path">
+    <van-tabbar
+      route
+      v-model="currentIndex"
+      active-color="#3cb490"
+      inactive-color="#000000"
+      placeholder
+    >
+      <van-tabbar-item
+        v-for="(item, index) in tabbarData"
+        :key="index"
+        :to="item.path"
+      >
         <span>{{ item.text }}</span>
         <template #icon>
           <!-- <img v-if="currentIndex === index" :src="getAssetUrl(item.imageActive)">
@@ -15,26 +25,20 @@
 </template>
 
 <script setup>
-import tabbarData from '@/assets/data/tabbar.js'
+import tabbarData from "@/assets/data/tabbar.js";
 // import { getAssetUrl } from '@/utils/get_img.js'
-import { ref, watch } from 'vue';
-import { useRoute } from 'vue-router'
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
 
-const currentIndex = ref(0)
+const currentIndex = ref(0);
 
-const route = useRoute()
+const route = useRoute();
 watch(route, (newRoute) => {
-  const findIndex = tabbarData.findIndex(item => item.path === newRoute.path)
-  console.log(findIndex);
-  if (findIndex === -1) return
-  currentIndex.value = findIndex
-
-})
-
-
-
-
-
+  const findIndex = tabbarData.findIndex((item) => item.path === newRoute.path);
+  // console.log(findIndex);
+  if (findIndex === -1) return;
+  currentIndex.value = findIndex;
+});
 </script>
 
 <style lang="less" scoped>
