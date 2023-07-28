f3d2d5a53017aec8a35cf380ca19d061cc368665
袁澧林 <kobe980918@gmail.com>
2023-07-28 17:15:38 +0800

    更新 favor.vue

--- src/views/favor/favor.vue
+++ src/views/favor/favor.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="favor">
     <h2>favor</h2>
-    <template v-for="(item) in 100" >
-      <div>列表{{ item }}</div>
-    </template>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style lang="less" scoped>
-.favor{
+.favor {
   height: 80vh;
   background-color: aquamarine;
   overflow-y: auto;
