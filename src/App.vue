47d076d00f9f9ec9e25fb149ddc21fd8559dc0fa
袁澧林 <kobe980918@gmail.com>
2023-07-28 17:15:16 +0800

    更新 App.vue
    保持home页面状态

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app">
-    <router-view></router-view>
+    <!-- name属性 -->
+    <router-view v-slot="props">
+      <keep-alive include="home">
+        <component :is="props.Component" />
+      </keep-alive>
+    </router-view>
+
     <!-- <tab-bar v-if="!route.meta.hideTabBar"></tab-bar> -->
     <tab-bar v-show="!route.meta.hideTabBar"></tab-bar>
 
@@ -10,11 +16,11 @@
 </template>
 
 <script setup>
-import { useRoute } from "vue-router"
-import TabBar from "./components/tab-bar/tab-bar.vue"
-import Loading from '@/components/loading/loading.vue'
+import { useRoute } from "vue-router";
+import TabBar from "./components/tab-bar/tab-bar.vue";
+import Loading from "@/components/loading/loading.vue";
 
-const route = useRoute()
+const route = useRoute();
 </script>
 
 <style scoped></style>
