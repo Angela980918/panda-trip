63ace0a977cbe0d4c1b93828aa83ff66e6cf0673
袁澧林 <kobe980918@gmail.com>
2023-07-28 17:15:40 +0800

    更新 home.vue

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home" ref="homeRef">
     <!-- 导航头部 -->
     <nav-bar></nav-bar>
 
@@ -22,47 +22,57 @@
   </div>
 </template>
 
+<script>
+export default { name: "home" };
+</script>
 <script setup>
-import NavBar from '@/views/home/cpns/nav-bar/nav-bar.vue'
-import HomeBanner from '@/views/home/cpns/home-banner/home-banner.vue'
-import HomeSearchBox from '@/views/home/cpns/home-searchbox/home-searchbox.vue'
-import HomeCategory from '@/views/home/cpns/home-category/home-category.vue'
-import HomeContent from '@/views/home/cpns/home-content/home-content.vue'
-import HomeSearchBar from '@/views/home/cpns/home-searchbar/home-searchbar.vue'
-import useHomeStore from '@/store/modules/home'
-import useScroll from '@/hooks/useScroll'
-import { watch, computed } from 'vue'
+import NavBar from "@/views/home/cpns/nav-bar/nav-bar.vue";
+import HomeBanner from "@/views/home/cpns/home-banner/home-banner.vue";
+import HomeSearchBox from "@/views/home/cpns/home-searchbox/home-searchbox.vue";
+import HomeCategory from "@/views/home/cpns/home-category/home-category.vue";
+import HomeContent from "@/views/home/cpns/home-content/home-content.vue";
+import HomeSearchBar from "@/views/home/cpns/home-searchbar/home-searchbar.vue";
+import useHomeStore from "@/store/modules/home";
+import useScroll from "@/hooks/useScroll";
+import { ref, watch, computed, onActivated } from "vue";
 
 // 发起网络请求
-const homeStore = useHomeStore()
+const homeStore = useHomeStore();
 homeStore.fetchAllCategoryData();
 homeStore.fetchAllHotData();
 homeStore.fetchAllHouse();
 
-const { isReachBottom, scrollTop } = useScroll();
+const homeRef = ref();
+const { isReachBottom, scrollTop } = useScroll(homeRef);
 // watch监听
 watch(isReachBottom, (newValue) => {
   if (newValue) {
     homeStore.fetchAllHouse().then(() => {
       // 等待数据请求完成
-      isReachBottom.value = false
-    })
+      isReachBottom.value = false;
+    });
   }
-})
+});
 
 // 搜索框显示的控制
 const showSearchBox = computed(() => {
-  return scrollTop.value >= 450
-})
+  return scrollTop.value >= 450;
+});
 
-
-
-
-
+// 跳转回home时, 保留原来的位置
+onActivated(() => {
+  homeRef?.value.scrollTo({
+    top: scrollTop.value,
+  });
+});
 </script>
 
 <style lang="less" scoped>
-.search{
+.home {
+  height: 100vh;
+  overflow-y: auto;
+}
+.search {
   position: fixed;
   top: 0;
   left: 0;
