8f74664fc1fe01e3767834446aeb72c0f15bfc92
袁澧林 <kobe980918@gmail.com>
2024-02-22 17:32:51 +0800

    Update components.d.ts

--- components.d.ts
+++ components.d.ts
@@ -7,17 +7,14 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    copy: typeof import('./src/components/home-house-item/house-item-v9 copy.vue')['default']
     HouseItemV3: typeof import('./src/components/home-house-item/house-item-v3.vue')['default']
     HouseItemV9: typeof import('./src/components/home-house-item/house-item-v9.vue')['default']
     Loading: typeof import('./src/components/loading/loading.vue')['default']
-    NavBar: typeof import('./src/components/nav-bar/nav-bar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TabBar: typeof import('./src/components/tab-bar/tab-bar.vue')['default']
     TabControl: typeof import('./src/components/tab-control/tab-control.vue')['default']
     VanBackTop: typeof import('vant/es')['BackTop']
-    VanButton: typeof import('vant/es')['Button']
     VanCalendar: typeof import('vant/es')['Calendar']
     VanCell: typeof import('vant/es')['Cell']
     VanIcon: typeof import('vant/es')['Icon']
